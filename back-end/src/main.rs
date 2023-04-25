761932a49c9b999d3fb54ea0fdd974c8735573a2
lknapik <logan.knapik@gmail.com>
2023-04-24 18:56:58 -0700

    Host static react files via rust

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,5 +1,8 @@
-use actix_web::{get, web, App, HttpServer, Responder, Result};
+use actix_web::{get, web, App, HttpServer, HttpRequest, Responder, Result};
+use actix_files::NamedFile;
 use serde::Serialize;
+use actix_web_lab::web::spa;
+use std::path::PathBuf;
 
 #[derive(Serialize)]
 struct MyObj {
@@ -14,10 +17,23 @@ async fn greet(name: web::Path<String>) -> Result<impl Responder> {
     Ok(web::Json(obj))
 }
 
+async fn index(_req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "../front-end/build/index.html".parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(greet)
+        App::new()
+        .service(greet)
+        .service(
+            spa()
+                .index_file("../front-end/build/index.html")
+                .static_resources_mount("/static")
+                .static_resources_location("../front-end/build/static/")
+                .finish(),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
