7bcf5d85286c513fa38eb3a38664e799965edac9
lknapik <logan.knapik@gmail.com>
2023-04-24 18:11:07 -0700

    Adding rust webserver backend

---
+++ back-end/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> Result<impl Responder> {
+    let obj = MyObj {
+        name: name.to_string(),
+    };
+    Ok(web::Json(obj))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
